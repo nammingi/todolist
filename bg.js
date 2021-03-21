da27694195cc4e983fb94201d45671ab28cc16a3
Nammingi <76784419+nammingi@users.noreply.github.com>
2021-03-21 19:28:56 +0900

    Update bg.js

--- bg.js
+++ bg.js
@@ -5,7 +5,7 @@ const IMG_NUMBER = 4;
 
 function paintImage(imgNumber){
     const image = new Image();
-    image.src = `images/${imgNumber + 1}.jpg`;
+    image.src = `https://nammingi.github.io/todolist/images/${imgNumber + 1}.jpg`;
     image.classList.add("bgImage")
     body.appendChild(image);
 }
